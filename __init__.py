6f8a51f98a81b62af4da85edcbfc179187ccaa21
lildieg971 <158208887+lildieg971@users.noreply.github.com>
2024-01-30 20:58:32 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
+
+@app.route("/fr/")
+def monfr():
+    return render_template('fr.html')
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
